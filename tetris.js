d3693ead29ed31da14cbdbb6471af969cb6dac65
NBY <151441189+tonguejob@users.noreply.github.com>
2024-04-21 12:59:01 +0300

    Update tetris.js

--- tetris.js
+++ tetris.js
@@ -12,7 +12,7 @@
   </style>
 </head>
 <body>
-  <canvas id="gameCanvas" width="20000" height="50000"></canvas>
+  <canvas id="gameCanvas" width="200" height="500"></canvas>
 
   <script>
     const canvas = document.getElementById('gameCanvas');
